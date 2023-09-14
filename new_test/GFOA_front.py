b7b1820f9259b64741633153e4581debf7e0c8e7
Junlang_Zhang <569328229@qq.com>
2023-09-14 23:02:50 +0800

    主函数，结合多线程、多进程控制，提交并发查单控制单独抢单进程版本

--- new_test/GFOA_front.py
+++ new_test/GFOA_front.py
@@ -2,29 +2,12 @@ from socket import socket,AF_INET,SOCK_STREAM
 import os,sys,subprocess,time
 from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor
 
-
+import GFOA_OP
 import tkinter
 
-from utils import tk_front,log_record
-logger =log_record.Logger(os.getcwd()+'/log/front.log',level='debug').logger
+from utils import tk_front
+from utils.log_record import logger
 
-def con_server(ip:str,port:int):
-    try:
-        sock = socket(AF_INET,SOCK_STREAM)
-        sock.connect((ip,port))
-    except Exception as e:
-        sock = None
-        logger.info(f"连接服务端异常:{e}")
-    finally:
-        return sock
-
-def send_msg(sock:socket,msg):
-    try:
-        sock.sendall(msg)
-        data = sock.recv(1024)
-        logger.info(f"收到消息{data}")
-    except Exception as e:
-        logger.info(f"前端socket消息异常:{e}")
 
 def taskkill(port):
     # 杀死原有的进程
@@ -66,9 +49,10 @@ def run_backend(port):
 
 def on_closing():
     logger.info("进程池结束")
-    p1.terminate()
+    # p1.terminate()
     p2.terminate()
     p3.terminate()
+    p4.terminate()
     # 关闭进程池
     # executor.shutdown()
     # 退出 tkinter 程序
@@ -78,21 +62,29 @@ def on_closing():
 ######################backend######################
 import json
 
-def echo_handler(address,client_sock:socket,flag):
+def echo_handler(address,client_sock:socket,flag,vartext:tkinter.StringVar,share_list):
     #处理客户端连接
     logger.info('Got connection from {}'.format(address))
     try:
         msg = client_sock.recv(8192)
         cont = msg.decode('utf8')
         if "开始抢单" in cont:
-            flag.value = True
             resend = '开始抢单成功'
             client_sock.sendall(resend.encode('utf8'))
             data = json.loads(cont.replace("开始抢单",""))
+            share_list.append(data)
             logger.info(f"开始抢单数据{data}")
+            
+            flag.value = True
         elif "暂停抢单" in cont:
             flag.value = False
             resend = '暂停抢单成功'
+            client_sock.sendall(resend.encode('utf8'))
+            vartext.set('')
+        elif '登录失效' in cont:
+            flag.value = False
+            vartext.set(cont)
+            resend = f'暂停抢单成功,{cont}'
             client_sock.sendall(resend.encode('utf8'))
     except Exception as e:
         logger.info(f"后端socket消息异常:{e}")
@@ -100,7 +92,7 @@ def echo_handler(address,client_sock:socket,flag):
         client_sock.close()
 
 
-def echo_server(address,q,backlog=5):
+def echo_server(address,flag,share_list,vartext,backlog=5):
     #启动socket服务端
     sock = socket(AF_INET,SOCK_STREAM)
     sock.bind(address)
@@ -108,7 +100,7 @@ def echo_server(address,q,backlog=5):
     while True:
         client_sock,client_addr = sock.accept()
         logger.info("接收到监听")
-        echo_handler(client_addr,client_sock,q)
+        echo_handler(client_addr,client_sock,flag,vartext,share_list)
 
 def test(flag):
     '''
@@ -123,28 +115,51 @@ def test(flag):
                 logger.info(i)
                 time.sleep(2)
 
+def check(flag,share_list,headers,port):
+    while True:
+        if flag.value:
+            data = share_list[0]
+            GFOA_OP.check(data['yyb_dict'],data['com_dic'],data['step_dic'],headers,port,flag)
+
+def snatch(flag,headers,port):
+    while True:
+        if flag.value:
+            for i in range(100):
+                GFOA_OP.snatch(headers,port,flag)
+            else:
+                flag.value = False
 
 
 if __name__ == "__main__":
     import multiprocessing
-    # from multiprocessing import freeze_support
-    # freeze_support()
-    share_val = multiprocessing.Value('b',False)
+    from threading import Thread
+    from multiprocessing import freeze_support
+    freeze_support()
+    #查询配置
+    share_list = multiprocessing.Manager().list()
+    #启停控制
+    check_flag = multiprocessing.Value('b',False)
+    snatch_flag = multiprocessing.Value('b',False)
     port = 20000
     taskkill(port)
-    p1 = multiprocessing.Process(target=echo_server,args=[('127.0.0.1',port),share_val])
-    p2 = multiprocessing.Process(target=test,args=[share_val])
-    p3 = multiprocessing.Process(target=test,args=[share_val])
-    p1.start()
+    header = GFOA_OP.get_headers()
+    p2 = multiprocessing.Process(target=check,args=[check_flag,share_list,header,port])
+    p4 = multiprocessing.Process(target=check,args=[check_flag,share_list,header,port])
+    p3 = multiprocessing.Process(target=snatch,args=[snatch_flag,header,port])
+    
     p2.start()
     p3.start()
+    p4.start()
     # with ThreadPoolExecutor(max_workers=2) as executor:
     #     app1 = executor.submit(echo_server,[('127.0.0.1',port),share_val])
     #     app2 = executor.submit(test,share_val)
     #     app3 = executor.submit(test,share_val)
     root=tkinter.Tk()
+    vartext = tkinter.StringVar()
+    t1 = Thread(target=echo_server,args=[('127.0.0.1',port),check_flag,share_list,vartext])
+    t1.start()
     root.title('GFOA')
     root.resizable(0,0)
-    tk_front.buju(root,port)
+    tk_front.buju(root,port,vartext)
     root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
